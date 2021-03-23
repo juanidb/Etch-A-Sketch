8f4ee76669ed0f9191ffba11c31b2afe8db661d0
Juan Diaz Bengolea <juanidb.95@gmail.com>
2021-03-23 02:11:33 -0300

    the basic grid is almost done

--- index.js
+++ index.js
@@ -2,13 +2,14 @@
 
 // funcion para crear el grid
 function newGrid(x, y){
-    for (let i = 0; i < (x*y); i++){
-        let gridEle = document.createElement('div');
-        gridEle.className = 'cell';
-        gridEle.innerHTML = '.';
-        container.appendChild(gridEle);
+    if (x<100&&y<100){
+        for (let i = 0; i < (x*y); i++){
+            let cell = document.createElement('div');
+            cell.className = 'cell';
+            cell.innerHTML = '';
+            container.appendChild(cell);
+        }
     }
-    container.style = ('height : 500px; width : 500px')
 }
 
 const body = document.querySelector('body');
@@ -17,7 +18,7 @@ const container = document.getElementById('container');
 for(let i = 0; i < (16*16); i++){
     let gridEle = document.createElement('div');
     gridEle.className = 'cell';
-    gridEle.innerHTML = '.';
+    gridEle.innerHTML = '';
     container.appendChild(gridEle);
 }
 
